08fd0465edd319d94cc48e11ad2ba154b8b581df
Ryan State <rwstate@gmail.com>
2019-09-30 17:58:18 -0400

    finish stretch stretch goals

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -4,17 +4,65 @@ let msHundredsLog = document.querySelector("#msHundreds");
 let sOnesLog = document.querySelector("#secondOnes");
 let sTensLog = document.querySelector("#secondTens");
 
-setInterval(function (){
-    if (timer < 1001) {
-        sTensLog.textContent = Math.floor(timer / 1000) % 10;
-        sOnesLog.textContent = Math.floor(timer / 100) % 10;
-        msHundredsLog.textContent = Math.floor(timer / 10) % 10;
-        msTens.textContent = timer % 10;
-        timer += 1
-    }
+let startButton = document.createElement('button');
+startButton.textContent = "Start";
 
-    else {
-        msTensLog.parentNode.classList.add("redDigit")
-        
+let resetButton = document.createElement('button');
+resetButton.textContent = "Reset";
+
+msTensLog.parentNode.appendChild(startButton);
+msTensLog.parentNode.appendChild(resetButton);
+
+let clickToStart = document.querySelectorAll('button')[0]
+let clickToReset = document.querySelectorAll('button')[1]
+
+var running = false
+
+let tenSecondTimer = function () {
+    let intervalId = setInterval(function (){
+        if (timer < 1001) {
+            sTensLog.textContent = Math.floor(timer / 1000) % 10;
+            sOnesLog.textContent = Math.floor(timer / 100) % 10;
+            msHundredsLog.textContent = Math.floor(timer / 10) % 10;
+            msTens.textContent = timer % 10;
+            timer += 1;
+        }
+
+        else {
+            msTensLog.parentNode.classList.add("redDigit");
+            clearInterval(intervalId)
+        }
+    }, 10);
+    clickToReset.addEventListener("click", function(){
+        clearInterval(intervalId)
+        sTensLog.textContent = "-";
+        sOnesLog.textContent = "-";
+        msHundredsLog.textContent = "-";
+        msTens.textContent = "-";
+        timer = 0
+        msTensLog.parentNode.classList.remove("redDigit");
+    
+    })
+    running = false;
+}
+
+clickToReset.addEventListener('click', function(){
+    sTensLog.textContent = "-";
+    sOnesLog.textContent = "-";
+    msHundredsLog.textContent = "-";
+    msTens.textContent = "-";
+    timer = 0
+})
+
+
+clickToStart.addEventListener("click", function() {
+    if (running === false) {
+        running = true
+        tenSecondTimer()
     }
-}, 10)
+})
+
+
+// tenSecondTimer()
+
+
