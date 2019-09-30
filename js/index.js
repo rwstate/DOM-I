e9520994a6188f9c9d3d575d585ebbda8e23183a
Ryan State <rwstate@gmail.com>
2019-09-30 15:49:15 -0400

    Add content through main section

--- js/index.js
+++ js/index.js
@@ -39,4 +39,30 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let links = document.querySelectorAll('a');
+links.forEach((link, i) => {
+  link.textContent = siteContent["nav"][`nav-item-${i + 1}`]
+});
+
+let headerText = document.querySelectorAll('.cta-text *');
+
+headerText.forEach((el, i) => {
+  el.textContent = Object.values(siteContent["cta"])[i]
+});
+
+let headerImg = document.querySelector("#cta-img").src = siteContent["cta"]["img-src"];
+
+let main = document.querySelectorAll(".text-content *");
+
+main.forEach((el, i) => {
+  if (i < 4) {
+    el.textContent = Object.values(siteContent["main-content"])[i];
+  }
+  else {
+    el.textContent = Object.values(siteContent["main-content"])[i + 1];
+  }
+});
+
+let middleImage = document.querySelector(".middle-img").src = siteContent["main-content"]["middle-img-src"];
